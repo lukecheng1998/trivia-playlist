34bf57d33f36915e472b79c4be48736717fca190
Luke Cheng <lukecheng@Lukes-MacBook-Pro.local>
2024-01-28 16:21:34 -0800

    more fixes

--- getsongs.py
+++ getsongs.py
@@ -1,13 +1,120 @@
 import requests
-# This file will get the songs and convert to a json file
-url = 'https://en.wikipedia.org/wiki/List_of_Billboard_Year-End_number-one_singles_and_albums'
-# Make a GET request to the URL
-response = requests.get(url)
+from bs4 import BeautifulSoup
+import json
+import time
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Print the content of the webpage
-    print(response.text)
-else:
-    # Print an error message if the request was not successful
-    print(f"Error: {response.status_code}")
+# This file will get the songs and convert to a json file
+# Make a GET request to the URL
+triviaPlaylist = "Trivia Playlist"
+def beginProcess(url):
+    response = requests.get(url)
+    songArray = scrapeArticle(response)
+    print(songArray)
+    return songArray
+
+def getTokenFromSpotify(authURL, clientId, clientSecrets, grantType, contentType):
+    # make a request here, obtains the auth token that is needed to execute commands in the spotify env
+    tokenResponse = requests.post(authURL, headers= {"Content-Type": contentType}, data={"grant_type": grantType, "client_id": clientId, "client_secret": clientSecrets}).json()
+    token = tokenResponse["access_token"]
+    token_type = tokenResponse["token_type"]
+    retToken = token_type + " " + token
+    return retToken
+def getPlaylistFromId(endpointURL, id, authHeader):
+    # method to get the ID of the playlist
+    # TODO: return errors properly rather than from methods
+    newEndpointURL = endpointURL + "/users/" + id + "/playlists"
+    response = requests.get(newEndpointURL, headers= {"Authorization": authHeader})
+    if response.status_code != 200:
+        error = "Could not get successful response for getting playlist"
+        return error
+    playlists = response.json()["items"]
+    # iterate through the playlist body to find the Trivia playlist
+    for playlist in playlists:
+        if playlist["name"] == triviaPlaylist:
+            print(playlist)
+            return playlist["id"]
+    error = "Playlist does not exist, please create the playlist"
+    return error
+
+def updateOrModifyPlaylist(endpointURL, playlistId, authHeader, songNames):
+    # after getting the playlist we need to see if the playlist needs to be changed or if songs need to be added to it
+    newEndpointURL = endpointURL + "/playlists/" + playlistId
+    response = requests.get(newEndpointURL, headers= {"Authorization": authHeader}).json()
+    tracks = response["tracks"]
+    if len(tracks["items"]) > 0:
+        # delete the tracks and reset
+        print(len(tracks["items"].length))
+    else:
+        # search for song ids here
+        searchAndReturnSongIds(endpointURL, authHeader, songNames)
+        print("Successfully added stuff to playlists")
+    return response
+
+def searchAndReturnSongIds(endpointURL, authHeader, songNames):
+    # searches for spotify's song Ids from the song/artist names listed
+    convertedSongNames = []
+    print("In search and return")
+    for i in range(0, len(songNames) - 1):
+        songName = songNames[i]
+        artistName = songNames[i + 1]
+        songArtistQuery = convertedSongNamesWithArtists(songName, artistName)
+        convertedSongNames.append(songArtistQuery)
+        i += 1
+    # TODO: change this URL
+    print(convertedSongNames)
+    for i in range(1, len(convertedSongNames)):
+        newEndpointURL = endpointURL + "/search?q=" + convertedSongNames[i]
+        print(newEndpointURL)
+        #TODO: figure out how to wait for the response to return on this URL
+        response = requests.get(newEndpointURL, headers={"Authorization": authHeader})
+        time.sleep(1)
+        if response.status_code != 200:
+            error = "didn't get a successful response please check the request"
+            return error
+
+        print(response)
+
+
+def convertedSongNamesWithArtists(songName, artistName):
+    #adds space between the song names
+    songName = songName.replace(" ", "%20")
+    artistName = artistName.replace(" ", "%20")
+    retQuery = "track%3A" + songName + "artist%3A" + artistName
+    return retQuery
+
+def convertArrayToJson(songArray):
+    with open('output.json', 'w', encoding='utf-8') as outfile:
+        json.dump(songArray, outfile, ensure_ascii=False, indent=4)
+    return json.dumps(songArray)
+
+
+def scrapeArticle(response):
+    # Check if the request was successful (status code 200)
+    html = ""
+    if response.status_code == 200:
+        # Print the content of the webpage
+        print("successfully obtained response")
+        html = response.text
+    else:
+        # Print an error message if the request was not successful
+        print(f"Error: {response.status_code}")
+    soup = BeautifulSoup(html, 'html.parser')
+    # Get the td value as the list of songs are in that section
+    td = soup.body.find_all(['td'])
+    if (td == None):
+        return []
+    # standardize the strings and then remove all the special characters
+    strings = str(td).lower()
+    strings = strings.replace("\\r", "")
+    strings = strings.replace("\\n", "")
+    stringArray = strings.split("\"")
+    # Get the song titles by obtaining the index next to the title=
+    indicies = [i + 1 for i, x in enumerate(stringArray) if x == ' title=']
+    songTitles = []
+    # Get the song titles
+    for i in indicies:
+        if 'billboard year-end ' in stringArray[i]:
+            continue
+        else:
+            songTitles.append(stringArray[i])
+    return songTitles
--- main.py
+++ main.py
@@ -2,10 +2,21 @@
 import getsongs
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+# FIELDS
+url = 'https://en.wikipedia.org/wiki/List_of_Billboard_Year-End_number-one_singles_and_albums'
+authURL = 'https://accounts.spotify.com/api/token'
+contentType = 'application/x-www-form-urlencoded'
+grantType = 'client_credentials'
+clientId = '555395fa580f41ff939897aff379da60'
+clientSecret = '98764b2630864555a86d119661a5736a'
+endpointURL = 'https://api.spotify.com/v1'
+username = '1256375693'
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    getsongs
+    songNames = getsongs.beginProcess(url)
+    token = getsongs.getTokenFromSpotify(authURL, clientId, clientSecret, grantType, contentType)
+    playlistId = getsongs.getPlaylistFromId(endpointURL, username, token)
+    getsongs.updateOrModifyPlaylist(endpointURL, playlistId, token, songNames)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
